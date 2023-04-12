bf9d34569b926cab48d44e54e1be853c23724d7e
Nic <101201789+nic5694@users.noreply.github.com>
2023-04-12 16:55:55 -0400

    added the databse creation files for the tables

--- backend/chatroombackend/database/migrations/2023_04_11_195036_create_messageGroup_table.php
+++ backend/chatroombackend/database/migrations/2023_04_11_195036_create_messageGroup_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messageGroup', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedInteger('Sender1_id');
+            $table->foreign('Sender1_id')
+                ->references('id')
+                ->on('users');
+            $table->unsignedInteger('Sender2_id');
+            $table->foreign('Sender2_id')
+                ->references('id')
+                ->on('users');
             $table->timestamps();
         });
     }
@@ -25,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('message_group');
     }
 };
---
+++ backend/chatroombackend/database/migrations/2023_04_12_193939_create_message_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('message');
+            $table->integer('sender_id')-> unsigned();
+            $table->foreign('sender_id')
+                ->references('id')
+                ->on('users');
+            $table->unsignedInteger('group_id');
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('messageGroup');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message');
+    }
+};
