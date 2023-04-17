d8ced211a668f6075e1c9108bc2c9be19098cd74
Youssef Chaboune <Chahbouney2000@outlook.fr>
2023-04-16 23:40:28 -0400

    .

--- frontend/chatroomfrontend/src/App.js
+++ frontend/chatroomfrontend/src/App.js
@@ -4,7 +4,10 @@ import NavBar2 from './components/NavBar2';
 function App() {
   return (
     <div>
+      Nav 1 (When logged in)
       <NavBar></NavBar>
+      <br></br>
+      Nav 2 (When Loging in or Registering)
       <NavBar2></NavBar2>
       {/*<NavBar2></NavBar2>*/}
     </div>
--- frontend/chatroomfrontend/src/components/NavBar.js
+++ frontend/chatroomfrontend/src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
             <div class="font-bold text-white mx-20 text-xl">
                 WorldChat <span class=" font-normal">|</span> <span class="text-[12px] font-normal">ChatBox</span>
             </div>
-            
+          
             <div class="mx-20">
                 <button class="duration-300 border-2 py-[3px] text-[12px] px-5 rounded-sm text-white font-semibold opacity-75 hover:opacity-100"> Log out </button>
             </div>
