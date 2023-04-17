69aa95965f6306f7fdbb62e05665711806b25008
Nic <101201789+nic5694@users.noreply.github.com>
2023-04-17 14:28:33 -0400

    updated database migration schema

--- backend/chatroombackend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/chatroombackend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('active');
             $table->binary('profile_picture')->nullable();
             $table->timestamps();
         });
