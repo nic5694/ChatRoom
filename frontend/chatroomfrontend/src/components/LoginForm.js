c1d2b31d2c77268b9ddfc42a6d7ea1c5532fe6cb
Youssef Chaboune <Chahbouney2000@outlook.fr>
2023-04-16 23:23:47 -0400

    created the files for the register and login form

---
+++ frontend/chatroomfrontend/src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function LoginForm() {
+    return (
+      <div>
+         
+      </div>
+    );
+  }
+  
+export default NavBar;
---
+++ frontend/chatroomfrontend/src/components/RegisterForm.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function RergisterForm() {
+    return (
+      <div>
+         
+      </div>
+    );
+  }
+  
+export default RergisterForm;
