bbf409493467c7b57ac99465217b93bfd2f52125
Nic <101201789+nic5694@users.noreply.github.com>
2023-04-17 14:57:16 -0400

    added controller and api
    Added user controller and api routes

--- backend/chatroombackend/app/Models/Message.php
+++ backend/chatroombackend/app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
@@ -16,4 +18,9 @@ class Message extends Model
         'timestamp'
     ];
     use HasFactory;
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
